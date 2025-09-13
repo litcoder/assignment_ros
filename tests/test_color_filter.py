328fb5f13bd4b1aa8e323ab95c98797f07b5dfc9
litcoder <litcoderwiz@gmail.com>
2025-09-13 21:49:29 +0900

    Add filter-wise test cases
    Signed-off-by: litcoder <litcoderwiz@gmail.com>

--- tests/test_color_filter.py
+++ tests/test_color_filter.py
@@ -1,3 +1,4 @@
+import numpy as np
 from src.color_filter import Filters
 
 
@@ -18,3 +19,76 @@ def test_kernels():
     assert "Sobel (x)".lower() in available_filters, "Sobel (X) filter"
     assert "Sobel (y)".lower() in available_filters, "Sobel (y) filter"
     assert "Edge detection".lower() in available_filters, "Edge detection filter"
+
+
+def get_filter(filters_dict: dict, filter_name: str):
+    """
+    A helper function to get data from given dictionary with case insensitive key
+    """
+    f = [v for k, v in filters_dict.items() if str(k).lower() ==
+         filter_name.lower()]
+    return f[0] if len(f) != 0 else None
+
+
+def test_original():
+    f = Filters()
+    filter = get_filter(f.Kernels, "original")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=np.float32))
+
+
+def test_blur():
+    f = Filters()
+    filter = get_filter(f.Kernels, "blur")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32) / 9)
+
+
+def test_gaussianblur():
+    f = Filters()
+    filter = get_filter(f.Kernels, "gaussian blur")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.float32) / 16)
+
+
+def test_sharpen():
+    f = Filters()
+    filter = get_filter(f.Kernels, "sharpen")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.float32))
+
+
+def test_sobelx():
+    f = Filters()
+    filter = get_filter(f.Kernels, "sobel (x)")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32))
+
+
+def test_sobely():
+    f = Filters()
+    filter = get_filter(f.Kernels, "sobel (y)")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32))
+
+
+def test_edgedetection():
+    f = Filters()
+    filter = get_filter(f.Kernels, "edge detection")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32))
+
+
+def test_emboss():
+    f = Filters()
+    filter = get_filter(f.Kernels, "emboss")
+    assert filter is not None
+    assert np.array_equal(filter, np.array(
+        [[-2, -1, 0], [-1, 1, 1], [0, 1, 2]], dtype=np.float32))
